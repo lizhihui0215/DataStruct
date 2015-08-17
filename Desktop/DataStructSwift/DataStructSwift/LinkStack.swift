2c4661fd68072d8078cb793995a39ced6102dfaf
lizhihui <pccw@pccwdeMac-mini.local>
2015-08-17 17:51:28 +0800

    add annotation to the linkStack :

--- Desktop/DataStructSwift/DataStructSwift/LinkStack.swift
+++ Desktop/DataStructSwift/DataStructSwift/LinkStack.swift
@@ -16,22 +16,45 @@ class LinkStack: NSObject {
         self.list = LinkList()
     }
     
+    /*!
+        clean the stack
+    */
     func emptyStack(){
         self.list?.makeEmpty()
     }
     
+    /*!
+    push the element to the stack
+    
+    :param: element the element to be pushed
+    */
     func push(element: AnyObject){
         self.list?.addNode(Node(element: element, next: nil))
     }
     
+    /*!
+    peek the top element
+    
+    :returns: the top element
+    */
     func top() -> AnyObject? {
         return self.list?.header.next
     }
     
-    func pop(){
+    /*!
+    pop the top element
+    */
+    func pop() -> Node?{
+        let node = self.list?.header.next
         self.list?.header.next = self.list?.header.next?.next
+        return node
     }
     
+    /*!
+    the description of the list stack
+    
+    :returns: the string of the list stack description
+    */
     func linkStackDescription() -> String? {
         return self.list?.listDescription()
     }
