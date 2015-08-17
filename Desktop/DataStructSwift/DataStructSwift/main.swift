08794eb812eb26e7ce00eea44fb537d53564e09b
lizhihui <pccw@pccwdeMac-mini.local>
2015-08-17 17:37:39 +0800

    linkstack

--- Desktop/DataStructSwift/DataStructSwift/main.swift
+++ Desktop/DataStructSwift/DataStructSwift/main.swift
@@ -12,11 +12,58 @@ var sortableArray = [1,23,5,6,1,2,6,75,3,1,23,732,432,7,12,43,6,3,56,23]
 
 var sortedArray = SortArithmetic.bubbleSortWith(sortableArray)
 
-print(sortedArray, appendNewline: true)
+println(sortedArray)
 
 var index = SortArithmetic.binarySearchWith(sortedArray: sortedArray, elementInArray: 732)
 
-print("the Element `\(sortedArray[index])` index of array is \(index)")
+println("the Element `\(sortedArray[index])` index of array is \(index)")
+
+
+var linkList = LinkList()
+
+linkList.addNode(Node(element: "1",next: nil))
+linkList.addNode(Node(element: "2",next: nil))
+linkList.addNode(Node(element: "3",next: nil))
+linkList.addNode(Node(element: "4",next: nil))
+linkList.addNode(Node(element: "5",next: nil))
+linkList.addNode(Node(element: "6",next: nil))
+linkList.addNode(Node(element: "7",next: nil))
+linkList.addNode(Node(element: "8",next: nil))
+
+println("linklist is \(linkList.listDescription()) the last node is \(linkList.lastNode().element)")
+
+linkList.delelteElement("4")
+
+println("linklist is \(linkList.listDescription())")
+
+var linkStack = LinkStack()
+
+linkStack.push("1")
+linkStack.push("2")
+linkStack.push("3")
+linkStack.push("4")
+linkStack.push("5")
+linkStack.push("6")
+
+println("linklist is \(linkStack.linkStackDescription())")
+
+linkStack.pop()
+linkStack.pop()
+linkStack.pop()
+
+println("linklist is \(linkStack.linkStackDescription())")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
